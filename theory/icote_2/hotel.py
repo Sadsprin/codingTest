f8f3c36e041ee0ea94295164432a56d3de86ade1
Sadsprin <54733868+Sadsprin@users.noreply.github.com>
2021-09-01 22:55:31 +0900

    Delete theory directory

--- theory/icote_2/hotel.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-N = int(sys.stdin.readline())
-
-for _ in range(N):
-    h, w, n = list(map(int, sys.stdin.readline().strip().split()))
-    ho, floor = divmod(n, h)
-    b = ho if floor == 0 else ho + 1  
-    print(str(floor if floor != 0 else str(h)) + ('0' + str(b) if b < 10 else str(b)))
--- theory/icote_2/sortstring.py
+++
@@ -1,8 +0,0 @@
-from collections import deque
-A = deque(sorted(list(input())))
-C = 0
-while A[0].isdigit():
-    B = A.popleft()
-    C += int(B)
-
-print("".join(list(A)) + str(C))
