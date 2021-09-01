9a2147713200f5adcc8418e4ce30e95ab516c0f3
Sadsprin <alswnssl0528@naver.com>
2021-09-01 12:09:40 +0900

    lrud.py added

---
+++ theory/icote_2/lrud.py
@@ -0,0 +1,24 @@
+import sys
+from pprint import pprint
+input = sys.stdin.readline
+
+N = int(input())
+move = input().rstrip().split()
+vector = {
+    'L' : (0, -1),
+    'R' : (0, 1),
+    'U' : (-1, 0),
+    'D' : (1, 0),
+}
+
+x, y = 0, 0
+twoDarray = [[0] * N for _ in range(N)]
+
+for move_ in move:
+    nx = x + vector[move_][0]
+    ny = y + vector[move_][1]
+    if nx < 0 or ny < 0 or nx > N - 1 or ny > N - 1:
+        continue
+    x, y = nx, ny
+
+print(x+1, y+1)
